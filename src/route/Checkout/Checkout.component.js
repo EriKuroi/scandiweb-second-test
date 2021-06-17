500891374a871267a30bf60d1c260b5d70dd2184
erikuroi <eryka.sh@gmail.com>
2021-06-17 21:39:48 +0300

    Override Checkout class to add progress bar, Override Checkout styles to add progress bar styles

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -1,7 +1,6 @@
 import { PureComponent } from 'react';
 import SourceCheckout from 'SourceRoute/Checkout/Checkout.component'
 import ContentWrapper from 'Component/ContentWrapper';
-import Test from '../../newComponents/Test/Test'
 // import our mimodified styles to override some of the default ones
 import './Checkout.override.style.scss';
 
@@ -14,43 +13,61 @@ export class Checkout extends SourceCheckout {
     stepProgressMap = {
         'SHIPPING_STEP': {
             title: 'Shipping',
+            number: '1',
         },
         'BILLING_STEP': {
             title: 'Billing & Payments',
+            number: '2',
+
         },
         'DETAILS_STEP': {
             title: 'Thank you!',
+            number: '3'
         }
     };
+    renderBreakpoint(step) {
+        const { title = '' } = this.stepProgressMap[step];
+        const { number = '' } = this.stepProgressMap[step];
+        const { checkoutStep } = this.props;
+        const currentNuber = this.stepProgressMap[checkoutStep].number;
+        const stepDone = currentNuber > number;
+
+        return (
+            <div className='breakpoint'>
+                <div className={stepDone ? 'numberPlace check' : 'numberPlace'}>
+                    {!stepDone &&
+                        <h4>
+                            {number}
+                        </h4>
+                    }
+                    {stepDone &&
+
+                        <svg x="0px" y="0px" viewBox="0 0 60 60" fill="white">
+                            <g>
+                                <path d="M26.375,39.781C26.559,39.928,26.78,40,27,40c0.276,0,0.551-0.114,0.748-0.336l16-18c0.367-0.412,0.33-1.045-0.083-1.411   c-0.414-0.368-1.045-0.33-1.412,0.083l-15.369,17.29l-9.259-7.407c-0.43-0.345-1.061-0.273-1.405,0.156   c-0.345,0.432-0.275,1.061,0.156,1.406L26.375,39.781z" />
+                            </g>
+                        </svg>
+
+                    }
+                </div>
+                <h4 className='breakpoint-title'>
+                    {title}
+                </h4>
+            </div>
+        )
+    }
     renderProgressBar() {
         const { checkoutStep } = this.props;
-        const { title = '' } = this.stepProgressMap[checkoutStep];
+        const isBilling = checkoutStep === 'BILLING_STEP'
+        const isDetails = checkoutStep === 'DETAILS_STEP'
         console.log(checkoutStep);
         return (
             <div className='Checkout-ProgressBar'>
-                <div className='sidebar'></div>
-                <div className='breakpoint'>
-                    <div className='numberPlace'>
-                        <h4>1</h4>
-                    </div>
-                <h4 className='breakpoint-title'>
-                    Shipping
-                </h4>
-                </div>
-                
-                <div className='middlebar'></div>
-                <div className='breakpoint'>
-                <div className='numberPlace'>
-                    <h4 >2</h4>
-                </div>
-
-                <h4 className='breakpoint-title billing'>
-                    Billing & Payments
-                </h4>
-                </div>
-                <div className='sidebar'></div>
-
-
+                <div className='sidebar done'></div>
+                {this.renderBreakpoint('SHIPPING_STEP')}
+                <div className={isBilling ? 'middlebar done' : 'middlebar'}></div>
+                {this.renderBreakpoint('BILLING_STEP')}
+                <div className={isBilling ? 'sidebar done' : 'sidebar'}></div>
             </div>
         );
     }
